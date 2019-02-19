670e30a763da6b940673b7fef76721a855a8920f
Jarbas Junior <jarbasoujunior@gmail.com>
2019-02-19 15:21:09 -0300

    Add sessions create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:sucess] = "Bem vindo #{user.username}"
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Usuario e/ou senha inválidos'
+      render new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logout realizado com sucesso."
+    redirect_to login_path
+  end
 end
